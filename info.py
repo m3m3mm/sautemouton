a0d2e940f48ee52b2504da7ad0f6b8735f7aa03a
emmanuil.kotelenets <wolrusteen@yandex.ru>
2026-04-08 12:45:01 +0200

    char

--- info.py
+++ info.py
@@ -2,10 +2,10 @@ with open("niveau_info.txt","r") as f:
     list = f.read().splitlines()
     f.close()
 
-personnage = {"position": (0,0), "speed": (0,0)}
+character = {"position": (0,0), "speed": (0,0)}
 lst_blocks = []
 print(list)
 
 position = list[0].split(", ",)
-personnage["position"] = (int(position[0]),int(position[1]))
-print(personnage)
+character["position"] = (int(position[0]),int(position[1]))
+print(character)
