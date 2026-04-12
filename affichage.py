4273a10784e20e97b35ec51d1ab187e75ed900d1
Emmanuel Kotelenets <wolrusteen@ya.ru>
2026-04-12 14:36:15 +0000

    ya debil, ubral svoy commentariy

--- affichage.py
+++ affichage.py
@@ -13,7 +13,6 @@ def draw_blocks(lst_blocks):
 
 def draw_final_object(goal):
     """draws final object that character needs to reach"""
-    # from Emmanuel - added second same value because it's a tuple
     x1, y1 = goal[0]
     x2, y2 = goal[1]
     rectangle(x1, y1, x2, y2)
