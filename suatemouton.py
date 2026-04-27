663eda6a05c9656811d941c5a98af21fef7cf1c3
Polina <polina.ivantaeva@edu.univ-eiffel.fr>
2026-04-28 01:23:30 +0200

    prikol

--- suatemouton.py
+++ suatemouton.py
@@ -1,7 +1,12 @@
 from interface import *
 from fltk import *
+import pygame
 
 cree_fenetre(width, height)
+
+pygame.mixer.init()
+pygame.mixer.music.load('Moonpetal.mp3')
+pygame.mixer.music.play(-1)
 
 start_game = draw_main_menu(width, height)
 if start_game:
