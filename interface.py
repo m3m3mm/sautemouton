4ad504ed38bb494e0d7008f43d97485739814b56
Emmanuel Kotelenets <wolrusteen@ya.ru>
2026-05-08 15:48:17 +0000

    solver alpha version only DFS

--- interface.py
+++ interface.py
@@ -1,12 +1,14 @@
 from affichage import *
 from physique import *
 from const_values import *
+from solver import *
+
 
 def game(character, lst_blocks, goal):
     """loop of the game which uses all the functions"""
     click = None
 
-    history = [character["position"]] # for backspace functionality we need the history of our positions
+    history = [character["position"]]  # for backspace functionality we need the history of our positions
 
     create_design()
     draw_character(character)
@@ -34,13 +36,13 @@ def game(character, lst_blocks, goal):
                 vx, vy = click_to_velocity(character, click)
                 character["velocity"] = (vx, vy)
                 efface("vector")
-                simulate(character, lst_blocks) # without this line mouton/barashek won't move
-                history.append(character["position"]) # saving positions
+                simulate(character, lst_blocks)  # without this line mouton/barashek won't move
+                history.append(character["position"])  # saving positions
 
                 click = None
-                if victory(character,goal):
+                if victory(character, goal):
                     draw_victory_menu(height, width)
-                    print("You won!") #placeholder for win message
+                    print("You won!")  # placeholder for win message
 
         if type_ev(ev) == 'Touche':
             if touche(ev) == 'BackSpace':
@@ -51,6 +53,53 @@ def game(character, lst_blocks, goal):
                     efface("shadow_of_step")
                     efface("mouton")
                     draw_character(character)
+
+            if touche(ev) == 'l':
+                saved_pos = character["position"]
+                saved_v = character["velocity"]
+
+                print("Solving with DFS ...")
+
+                dolly = {"position": saved_pos, "velocity": (0, 0)}
+
+                path = dfs(dolly, lst_blocks, goal)
+
+                character["position"] = saved_pos
+                character["velocity"] = saved_v
+
+                if path is None:
+                    print("Solution not found!")
+
+                else:
+                    print("Solution found")
+
+                    efface("solver_path")
+                    efface("solver_character")
+
+                    dolly = {"position": saved_pos,"velocity": (0, 0)}
+
+                    for vx, vy in path:
+                        dolly["velocity"] = (vx, vy)
+
+                        while True:
+                            old_pos = dolly["position"]
+
+                            solver_step(dolly, lst_blocks)
+
+                            x, y = dolly["position"]
+
+                            cercle(x + WIDTH / 2,y + HEIGHT / 2, 3, couleur="yellow", remplissage="yellow", tag="solver_path")
+
+                            efface("solver_character")
+
+                            cercle(x + WIDTH / 2, y + HEIGHT / 2, WIDTH / 2, couleur="orange", remplissage="orange", tag="solver_character")
+
+                            mise_a_jour()
+
+                            sleep(0.01)
+
+                            if dolly["position"] == old_pos:
+                                break
 
         efface('mouton')
         draw_character(character)
--- solver.py
+++ solver.py
@@ -1,1 +1,80 @@
-# TODO
+from const_values import *
+from fltk import *
+from physique import *
+from info import goal
+from time import sleep
+
+def solver_step(character, lst_blocks):
+    current_x, current_y = character["position"]
+    vx, vy  = character["velocity"]
+    new_x = current_x + STEP * vx
+    new_y = current_y + STEP * vy
+
+    new_vx = vx + STEP * gravity_x
+    new_vy = vy + STEP * gravity_y
+
+    character["position"] = (new_x, new_y)
+    character["velocity"] = (new_vx, new_vy)
+
+    hit(character, lst_blocks)
+
+def solver_simulate(character, lst_blocks):
+    max_steps = 1000
+    steps = 0
+
+    while steps < max_steps:
+        old_pos = character["position"]
+
+        solver_step(character, lst_blocks)
+
+        if character["position"] == old_pos:
+            return
+
+        steps += 1
+
+def get_velocities():
+    velocities = []
+    step = 3
+    for vx in range(-vmax, vmax + 1, step):
+        for vy in range(-vmax, vmax + 1, step):
+            velocities.append((vx, vy))
+    return velocities
+
+global_velocities = get_velocities()
+
+def roundup_pos(position):
+    return (int(position[0]) // POSITION_R, int(position[1]) // POSITION_R)
+
+"""DFS solution"""
+
+def dfs(character, lst_blocks, goal, visited=None, path=None):
+    if visited is None:
+        visited = set()
+    if path is None:
+        path = []
+
+    if victory(character, goal):
+        return path[:] # returning the path if won
+
+    pos = roundup_pos(character["position"])
+    if pos in visited:
+        return None
+    visited.add(pos)
+
+    for vx, vy in global_velocities:
+        saved_pos = character["position"]
+        saved_v =  character["velocity"]
+
+        character["velocity"] = (vx,vy)
+        solver_simulate(character, lst_blocks)
+
+        path.append((vx,vy))
+        result = dfs(character,lst_blocks,goal,visited,path)
+        if result is not None:
+            return result
+        path.pop()
+
+        character["position"] = saved_pos
+        character["velocity"] = saved_v
+
+    return None
