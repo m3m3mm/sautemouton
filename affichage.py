9330132138ea16180f7a37f38ec52debc4ee6e8f
Polina Ivantaeva <polina.ivantaeva@edu.univ-eiffel.fr>
2026-05-08 20:14:49 +0200

    new level

--- affichage.py
+++ affichage.py
@@ -2,12 +2,14 @@ from const_values import WIDTH, HEIGHT
 from info import *
 from fltk import *
 
+
+
 def draw_blocks(lst_blocks):
     """draws blocks"""
     for block in lst_blocks:
         x1, y1, x2, y2 = block
         #rectangle(x1, y1, x2, y2, remplissage="red")
-        image(x1, y1, "block.png", largeur = x2 - x1,
+        image(x1, y1, "design/block.png", largeur = x2 - x1,
               hauteur = y2 - y1, ancrage="nw")
 
 
@@ -15,7 +17,7 @@ def draw_final_object(goal):
     """draws final object that character needs to reach"""
     x1, y1, x2, y2 = goal
     #rectangle(x1, y1, x2, y2, remplissage="white")
-    image(x1, y1, 'chest.png', largeur = x2 - x1, hauteur = y2 - y1, ancrage="nw")
+    image(x1, y1, 'design/chest.png', largeur = x2 - x1, hauteur = y2 - y1, ancrage="nw")
 
 
 
@@ -33,18 +35,18 @@ def draw_victory_menu(height, width):
 
 def create_design():
     """creates design"""
-    image(0, 0, "background.ppm", ancrage="nw")
+    image(0, 0, "design/background.ppm", ancrage="nw")
 
 def draw_main_menu(width, height):
     """draws main menu"""
     image(width / 2, height / 2,
-          "main_menu.png",
+          "design/main_menu.png",
           largeur=width,
           hauteur=height,
           ancrage="center", tag="main_menu")
     #rectangle(50, 170, 190, 220, remplissage="red")
-    image(width/7, height/2 - 30, 'startgame.png', largeur=140, hauteur=50 , ancrage="nw", tag="start")
-    image(width/7, height/2 + 40, 'option.png', largeur=140, hauteur=50 , ancrage="nw", tag="levels")
+    image(width/7, height/2 - 30, 'design/startgame.png', largeur=140, hauteur=50 , ancrage="nw", tag="start")
+    image(width/7, height/2 + 40, 'design/option.png', largeur=140, hauteur=50 , ancrage="nw", tag="levels")
 
     while True:
 
@@ -56,6 +58,7 @@ def draw_main_menu(width, height):
             click = (abscisse(ev), ordonnee(ev))
             if (width/7) <= click[0] <= (width/7 + 140):
                 if (height/2-30) <= click[1] <= (height/2 + 20):
+                    print('started isb true')
                     started = True
                     selected_level = 'niveau_info.txt'
                     return started, selected_level
@@ -65,7 +68,24 @@ def draw_main_menu(width, height):
                     efface('levels')
                     rectangle(67, 198, 177, 240, remplissage='red')
                     rectangle(67, 249, 177, 290, remplissage='red')
-                    image(width/7, height/2 - 30, 'levels.png', largeur=140, hauteur=160 , ancrage="nw", tag="levels")
+                    image(width/7, height/2 - 30, 'design/levels.png', largeur=140, hauteur=160 , ancrage="nw", tag="levels")
+
+                    ev2 = attend_ev()
+                    if type_ev(ev2) == 'ClicGauche':
+                        click = (abscisse(ev2), ordonnee(ev2))
+                        if 67 <= click[0] <= 177:
+                            if 198 <= click[1] <= 240:
+                                print('first level')
+                                started = True
+                                selected_level = 'niveau_info.txt'
+                                return started, selected_level
+                            else:
+                                print('second level')
+                                started = True
+                                selected_level = 'second_level.txt'
+                                return started, selected_level
+
+
 
 
 
--- suatemouton.py
+++ suatemouton.py
@@ -1,6 +1,7 @@
 from interface import *
 from fltk import *
 import pygame
+from const_values import *
 
 cree_fenetre(width, height)
 
@@ -11,6 +12,7 @@ pygame.mixer.music.set_volume(0.3)
 
 start_game, selected_level = draw_main_menu(width, height)
 if start_game:
+    character, lst_blocks, goal = open_file(selected_level)
     game(character, lst_blocks, goal)
 else:
     character, lst_blocks, goal = open_file(selected_level)
