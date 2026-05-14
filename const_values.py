821d77ecf92b556b9f218b271c93a152d228d02c
Emmanuel Kotelenets <wolrusteen@ya.ru>
2026-05-14 12:09:08 +0000

    new grav settings

--- const_values.py
+++ const_values.py
@@ -2,8 +2,8 @@ height = 400
 width = 345
 vmax = 15
 gravity_x = 0 # A priori, la gravité est strictement verticale (gx = 0) (from file of the project)
-gravity_y = 1.25
+gravity_y = 1.2
 WIDTH, HEIGHT = 20, 20
-STEP = 0.35 # "1" is too much
+STEP = 0.5 # "1" is too much
 POSITION_R = 5
 """to adjust the game - change STEP and gravity_y"""
