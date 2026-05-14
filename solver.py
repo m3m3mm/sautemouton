71f424d76963866e880397427321c514c3f13e67
Emmanuel Kotelenets <wolrusteen@ya.ru>
2026-05-14 18:52:26 +0000

    dfs fixed

--- solver.py
+++ solver.py
@@ -4,14 +4,16 @@ from physique import *
 from info import goal
 from time import sleep
 
+SOLVER_STEP = 0.3
+
 def solver_step(character, lst_blocks):
     current_x, current_y = character["position"]
     vx, vy  = character["velocity"]
-    new_x = current_x + STEP * vx
-    new_y = current_y + STEP * vy
+    new_x = current_x + SOLVER_STEP * vx
+    new_y = current_y + SOLVER_STEP * vy
 
-    new_vx = vx + STEP * gravity_x
-    new_vy = vy + STEP * gravity_y
+    new_vx = vx + SOLVER_STEP * gravity_x
+    new_vy = vy + SOLVER_STEP * gravity_y
 
     character["position"] = (new_x, new_y)
     character["velocity"] = (new_vx, new_vy)
@@ -20,23 +22,21 @@ def solver_step(character, lst_blocks):
 
 def solver_simulate(character, lst_blocks):
     max_steps = 1000
-    steps = 0
 
-    while steps < max_steps:
+    for _ in range(max_steps):
+        old_pos = character["position"]
+        solver_step(character, lst_blocks)
+        if character["position"] == old_pos:
+            character["velocity"] = (0, 0)
+            return
+    character["velocity"] = (-1, -1)  # for is_valid_state
 
-        for _ in range(max_steps):
-            old_pos = character["position"]
-            solver_step(character, lst_blocks)
-            if character["position"] == old_pos:
-                character["velocity"] = (0, 0)
-                return
-        character["velocity"] = (-1, -1)  # for is_valid_state
 
 
 def get_velocities():
     velocities = []
-    step = 3
-    for vx in range(-vmax, vmax + 1, step):
+    step = 2
+    for vx in range(int(-vmax), vmax + 1, step):
         for vy in range(-vmax, vmax + 1, step):
             velocities.append((vx, vy))
     return velocities
@@ -46,20 +46,27 @@ global_velocities = get_velocities()
 def roundup_pos(position):
     return (int(position[0]) // POSITION_R, int(position[1]) // POSITION_R)
 
-def is_valid_state(character, lst_blocks):
-    x, y = character['position']
-    vx, vy = character['velocity']
+def is_grounded(character, lst_blocks):
 
-    # if the sheep is still moving
+    x, y = character['position']
+    below = {'position': (x, y + 1), 'velocity': (0, 0)}
+    return collision(below, lst_blocks) is not None
+
+
+def is_valid_state(character, lst_blocks):
+    vx, vy = character['velocity']
     if (vx, vy) != (0, 0):
         return False
 
-    # if the sheep is stuck in a wall/block
     if collision(character, lst_blocks) is not None:
         return False
 
-    # if SOMEHOW our sheep is out of bounds
+    x, y = character['position']
     if x < 0 or y < 0 or x > width or y > height:
+        return False
+
+    # if the sheep is not on ground, but stuck in the air (glued to a wall or smth)
+    if not is_grounded(character, lst_blocks):
         return False
 
     return True
